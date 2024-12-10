44b9e0b8161f8ff20545f98eceeb4b005566ecf4
vibhore tiwari <vibhore.tiwari@coraltele.com>
2024-12-10 15:16:46 +0530

    configuration is change

--- inventory/src/main/java/com/example/inventory/InventoryApplication.java
+++ inventory/src/main/java/com/example/inventory/InventoryApplication.java
@@ -2,9 +2,10 @@ package com.example.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-
+@EnableEurekaClient
 public class InventoryApplication {
 
 	public static void main(String[] args) {
